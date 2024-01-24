6e4481385098dd264b5987dc5d35f11a8a52a219
Tian Xia <xidich@gmail.com>
2024-01-24 16:35:26 -0500

    Working periodic update

--- realtime.py
+++ realtime.py
@@ -6,33 +6,30 @@ import pandas as pd
 from random import randint
 import plotly.express as px
 
+from functools import partial
+
 pn.extension(sizing_mode="stretch_both")
 pn.extension('plotly')
 
 ACCENT_BASE_COLOR = "#00693e"
 
-
-
-
 df = pd.DataFrame(columns=["Time", "Value"])
 streaming_component = px.line(data_frame=df)
 panel = pn.pane.Plotly(streaming_component)
 
-def update():
+def update(source):
     time = datetime.datetime.utcnow()
     val = randint(0,100)
-    print(time,val)
-    newrow = {"Time":time,"Value":val}
+    new = [time, val]
+    new = pd.DataFrame(columns = source.columns, data = [new])
+    source = pd.concat([source, new], axis = 0)
+    print(source)
 
-    df = df.append(newrow)
-
-    print(df)
-
-pn.state.add_periodic_callback(callback=update, period=50)
+pn.state.add_periodic_callback(callback=partial(update, df), period=50)
 
 template = pn.template.FastListTemplate(
     site="DFR GUI",
-    title="Streaming using periodic callback",,
+    title="Streaming using periodic callback",
     header_background=ACCENT_BASE_COLOR,
     accent_base_color=ACCENT_BASE_COLOR,
     main=[panel],
