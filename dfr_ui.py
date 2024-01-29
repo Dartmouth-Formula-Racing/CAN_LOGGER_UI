8611a7ca4635d77ec4909fdefc3da00280175d1a
Nestor Orozco-Llamas <norozcollamas@middlebury.edu>
2024-01-29 14:57:04 -0500

    Added interpolated button for csv export

--- dfr_ui.py
+++ dfr_ui.py
@@ -4,28 +4,27 @@ import pandas as pd
 import CANverter as canvtr
 from tkinter.filedialog import askopenfilename, asksaveasfilename
 from tkinter import Tk
-import hvplot.pandas
 import plotly.graph_objects as go
-import traceback
 
 
 pn.extension('plotly')
 pn.extension('floatpanel')
 
 time_field = 'Time (ms)'
-whole_df = pd.DataFrame(columns=[time_field, 'y'])
+current_dataframe = pd.DataFrame(columns=[time_field, 'y'])
 canverter = None
 log_file_path = ''
 dbc_file_path = ''
 csv_file_path = ''
 project_options = [proj.split(".")[0] for proj in os.listdir("./PROJECTS/")]
+current_project_name = ''
 
 pn.extension(template='fast')
 
 Tk().withdraw()
 root_path = os.path.expanduser("~")
 
-pn.state.template.title = 'DFR UI'
+pn.state.template.title = 'Dartmouth Formula Racing CAN Explorer'
 
 
 def graphing():
@@ -33,7 +32,7 @@ def graphing():
     # y: list of attributes from csv we want to graph (dataframe headers)
 
     # Only handles up to 4...
-    global whole_df
+    global current_dataframe
     ys = column_select_choice.value
 
     fig = go.Figure()
@@ -44,15 +43,15 @@ def graphing():
     i=1
     for column_name in ys:
         fig.add_trace(go.Scatter(
-            x=whole_df[time_field],
-            y=whole_df[column_name],
+            x=current_dataframe[time_field],
+            y=current_dataframe[column_name],
             name=column_name,
             yaxis=f"y{i}"
         ))
         i=i+1
     
     fig.update_layout(
-        xaxis=dict(domain=[0.2, 0.8], title="Time")
+        xaxis=dict(domain=[0.2, 0.8], title=time_field)
     )
 
     signal_num = len(ys)
@@ -102,19 +101,26 @@ def graphing():
 
     return fig
 
-def build_whole_df(selected_file_path, project_name):
-    global whole_df
+def build_current_dataframe(selected_file_path, project_name):
+    global current_dataframe
     global canverter
 
-    whole_df = canverter.log_to_dataframe(selected_file_path)
-    whole_df = whole_df.sort_values(by=time_field)
-    whole_df.to_pickle("./PROJECTS/"+project_name+".project")
+    current_dataframe = canverter.log_to_dataframe(selected_file_path)
+    current_dataframe = current_dataframe.sort_values(by=time_field)
+    current_dataframe.to_pickle("./PROJECTS/"+project_name+".project")
 
+    interpolate_dataframe()
+    
+
+def interpolate_dataframe():
+    global current_dataframe
+    
     # Interpolate all columns linearly based on the "time" column
-    whole_df = whole_df.interpolate(method='linear', axis=0)
-    all_columns = whole_df.columns.tolist()
+    current_dataframe = current_dataframe.interpolate(method='linear', axis=0)
+    all_columns = current_dataframe.columns.tolist()
     all_columns.remove(time_field)
     column_select_choice.options = all_columns
+    
 
 def dbc_file_picker_callback(event):
     global dbc_file_path
@@ -130,7 +136,7 @@ def data_file_picker_callback(event):
     
 def import_data_callback(event):
     global canverter
-    global whole_df
+    global current_dataframe
     global log_file_path
     if len(file_selection[-1]) > 1:
         file_selection[-1].pop(1)
@@ -145,7 +151,7 @@ def import_data_callback(event):
                 if (canverter == None):
                     print('Handle case later')
                 else:
-                    build_whole_df(log_file_path, project_name)
+                    build_current_dataframe(log_file_path, project_name)
                     project_name_select.options = [proj.split(".")[0] for proj in os.listdir("./PROJECTS/")]
                     project_name_input_text.value = ""
             elif (file_extension == 'pkl'):
@@ -158,36 +164,37 @@ def import_data_callback(event):
     except Exception as e:
         file_selection[-1][-1]= ("Importing Failed!")
 
-
-    
 def csv_file_picker_callback(event):
     global csv_file_path
     csv_file_path = asksaveasfilename(title = "Save Exported CSV File", filetypes = (("CSV Files","*.csv"),("all files","*.*")))
     csv_export_text.value = csv_file_path
 
 def save_csv_callback(event):
-    global whole_df
+    global current_dataframe
     global csv_file_path
+    global current_project_name
     csv_file_path = csv_export_text.value
     if len(csv_export_selection[-1]) > 1:
         csv_export_selection[-1].pop(1)
     csv_export_selection[-1].append("Exporting Data...")
     try:
-        whole_df.to_csv(csv_file_path)
+        if interpolate_csv_btn.value:
+            current_dataframe.to_csv(csv_file_path)
+        else:
+            pd.read_pickle("./PROJECTS/"+current_project_name+".project").to_csv(csv_file_path)
         csv_file_path = ''
         csv_export_selection[-1][-1]= ("Export Successful!")
     except:
         csv_export_selection[-1][-1]= ("Export Failed!")
         
-    
 def export_data_panel(event):
     csv_export_text.placeholder = root_path
-    user_input_block.append(pn.layout.FloatPanel(csv_export_selection, name='Export to CSV', height=300, width=500, contained=False, position="center"))
+    layout.append(pn.layout.FloatPanel(csv_export_selection, name='Export to CSV', height=300, width=500, contained=False, position="center"))
     if len(csv_export_selection[-1]) > 1:
         csv_export_selection[-1].pop(1)
     
 def import_data_panel(event):
-    user_input_block.append(pn.layout.FloatPanel(file_selection, name='Import Data', height=400, width=500, contained=False, position="center"))
+    layout.append(pn.layout.FloatPanel(file_selection, name='Import Data', height=400, width=500, contained=False, position="center"))
     project_name_input_text.placeholder = "Project Name..."
     dbc_file_input_text.placeholder = root_path
     log_file_input_text.placeholder = root_path
@@ -200,11 +207,12 @@ choose_csv_file_btn = pn.widgets.Button(name='Choose a file', height=50,align='c
 choose_csv_file_btn.on_click(csv_file_picker_callback)
 save_csv_button = pn.widgets.Button(name='Save', height=50)
 save_csv_button.on_click(save_csv_callback)
-export_data_panel_btn = pn.widgets.Button(name='Export Data', height=50)
+export_data_panel_btn = pn.widgets.Button(name='Export Data', align="center",height = 50)
 export_data_panel_btn.on_click(export_data_panel)
+interpolate_csv_btn = pn.widgets.Checkbox(name='Interpolate Data')
 
 # IMPORT_DATA FUNCTION WIDGETS
-import_data_panel_btn = pn.widgets.Button(name='Import Data', height=50)
+import_data_panel_btn = pn.widgets.Button(name='Import Data',align="center",height = 50)
 import_data_panel_btn.on_click(import_data_panel)
 dbc_file_btn = pn.widgets.Button(name='Upload .dbc file', height=50)
 dbc_file_btn.on_click(dbc_file_picker_callback)
@@ -217,30 +225,29 @@ import_data_button = pn.widgets.Button(name='Import Project', height=50)
 import_data_button.on_click(import_data_callback)
 
 #SELECT PROJECT FUNCTION WIDGETS
-project_name_select = pn.widgets.Select(name='Select Project',options=project_options)
+project_name_select = pn.widgets.Select(name='Select Project',options=project_options, align="center")
 @pn.depends(project_name_select.param.value, watch=True)
 def update_project(project_name_select):
-    global whole_df
+    global current_dataframe
+    global current_project_name
     column_select_choice.name = "Variables for "+project_name_select
     column_select_choice.value = []
-    whole_df = pd.read_pickle("./PROJECTS/"+project_name_select+".project")
+    current_dataframe = pd.read_pickle("./PROJECTS/"+project_name_select+".project")
      # Interpolate all columns linearly based on the "time" column
-    whole_df = whole_df.interpolate(method='linear', axis=0)
-    all_columns = whole_df.columns.tolist()
-    all_columns.remove(time_field)
-    column_select_choice.options = all_columns
+    interpolate_dataframe()
+    current_project_name = project_name_select
   
 column_select_choice = pn.widgets.MultiChoice(name="Variables for "+project_name_select.value, value=[],
-    options=[])
+    options=[], align="center")
 
 def clear_all_columns(event):
     column_select_choice.value = []
 
-clear_all_columns_btn = pn.widgets.Button(name='Clear all columns', height=50)
+clear_all_columns_btn = pn.widgets.Button(name='Clear all columns', height=50, align="center")
 clear_all_columns_btn.on_click(clear_all_columns)
 
 def generate_plot(event):
-    global whole_df
+    global current_dataframe
     try:
         plotly_pane.object = graphing()
     except Exception as ex:
@@ -251,7 +258,7 @@ figure = graphing()
 
 plotly_pane = pn.pane.Plotly(figure)
 
-generate_plot_btn = pn.widgets.Button(name='Generate plot', height=50)
+generate_plot_btn = pn.widgets.Button(name='Generate plot', height=50, align="center")
 generate_plot_btn.on_click(generate_plot)
 
 file_selection = pn.Column(
@@ -277,25 +284,29 @@ csv_export_selection = pn.Column(
         "Select the project you want to export into a .csv",
         pn.Row(project_name_select),
         pn.Row(csv_export_text, choose_csv_file_btn),
+        pn.Row(interpolate_csv_btn),
         pn.Row(save_csv_button)
     )
 export_selection = pn.Row(
+        pn.layout.VSpacer(),
         project_name_select,
         import_data_panel_btn,
         export_data_panel_btn,
-        height=100
+        pn.layout.VSpacer(),
+        height=80
     )
 
 plot_generation = pn.Row(
+    pn.layout.VSpacer(),
     column_select_choice,
     clear_all_columns_btn,
     generate_plot_btn,
-    height=200
+    pn.layout.VSpacer(),
+    height=80
 )
 
 plot_display = pn.Row(
     plotly_pane,
-    height=400
 )
 
 user_input_block = pn.Column(
@@ -303,7 +314,7 @@ user_input_block = pn.Column(
     plot_generation,
 )
 
-layout = pn.Row(
+layout = pn.Column(
     user_input_block,
     plot_display
 )
